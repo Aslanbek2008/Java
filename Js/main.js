6e4f2b0da2cc6a208080ad5df565928d4502acda
Aslanbek2008 <aslanbekodilov80@gmail.com>
2024-03-29 20:50:48 +0500

    Java 1.2

--- Js/main.js
+++ Js/main.js
@@ -1,19 +1,56 @@
 
 
-var ellist = document.querySelector(".list");
-var elitem = document.querySelector(".item");
-var eltext = document.querySelector(".text")
+// var ellist = document.querySelector(".list");
+// var elitem = document.querySelector(".item");
+// var eltext = document.querySelector(".text")
 
-ellist.addEventListener("submit" , function(evt) {
+// var cal = 2;
+
+// ellist.addEventListener("submit" , function(evt) {
+//     evt.preventDefault();
+
+//     var input = elitem.value.trim()
+
+//     var Hisob = input * cal;
+
+//     if(input) {
+//       eltext.textContent = Hisob;
+//     }
+// });
+
+
+// var elList = document.querySelector(".list");
+// var elItem = document.querySelector(".item");
+// var elText = document.querySelector(".text");
+
+// function ChangeEnter(evt) {
+//   evt.preventDefault();
+
+//   var input = elItem.value.trim()
+
+//   elText.textContent = input;
+// }
+
+// elList.addEventListener("submit" , ChangeEnter);
+
+
+var elbutton = document.querySelector(".button");
+var elbuttonka = document.querySelector(".buttonka");
+var elList = document.querySelector(".list");
+
+
+function ButtonSubmit(evt) {
     evt.preventDefault();
 
-    var input = elitem.value.trim()
-
-    console.log("item");
-
-    if(input) {
-      eltext.textContent = input;
-    }
-});
+    elList.classList.toggle("open");
+}
 
 
+function Buttobka(evt) {
+    evt.preventDefault();
+
+    elList.classList.toggle("open");
+}
+
+  elbutton.addEventListener("click", ButtonSubmit);
+  elbuttonka.addEventListener("click", Buttobka);
